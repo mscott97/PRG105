9e4b66b9c3973074ae0ce54a6df64a5fe987d0f8
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-09-06 09:38:41 -0500

    Ch2

---
+++ ch_2_practice.py
@@ -0,0 +1,72 @@
+
+
+print("Mariya Scott")
+print("Computer Science With a Minor In App Design")
+print('the Dog Says /woof/')
+
+
+
+
+age = 19
+print(age)
+print("age " + str(age))
+print ("age", age)
+age = 19
+print(age)
+print("age " + str(age))
+print ("age", age)
+
+
+name= input("Please print your name:")
+print ("Hello " + name)
+
+
+age= int(input("please enter your age:"))
+print("This year you are ", age)
+age = age + 1
+print("next year you will be " + str(age))
+#   Sample:
+#   Write an equation that adds an integer to an integer, display the equation and the result with a print statement
+answer = 7/2
+print("7 / 2 = " , answer)
+remainder = 7 % 2
+print ("The remainder = 7 / 2")
+
+print ("6/3 = " + str(6/3))
+print ("2.5 / .7 = " +str(2.5/2))
+print ("2.5 / 2 = " + str(2.5/2))
+
+# TODO 2.8 Output
+#   modify the following code to print on one line, without merging the lines of code. Separate the words with a hyphen
+#   DO NOT MERGE INTO ONE LINE OF CODE, use escape characters
+print('one', end=" ")
+print('two', end=" ")
+print('three')
+
+# TODO 2.8 Output
+#   Modify the following line of code to add tabs between the days of the week
+print("Sunday, Monday Tuesday Wednesday Thursday Friday Saturday", sep=("/t)
+
+
+# TODO 2.8 Concatenating strings (Displaying Multiple Items with the + Operator)
+#   Have the user enter their name
+#   Greet the user, concatenate hello and their name into one string
+
+
+# TODO 2.8 Formatting numbers
+#   Change the output so that it is formatted to display a minimum field width of 30
+#   include commas, with only
+#   two numbers showing to the right of the decimal point
+#   example:
+#                 6,548,974,897.57
+number = 6548974897.5687979797
+print(number)
+
+
+# TODO 2.8 Formatting percentage
+#   Format the following as a percentage with 2 as the precision
+#   Sample:
+#       percent = .25834
+#       print(format(percent, '%'))
+percentage = .7654
+print(percentage)
