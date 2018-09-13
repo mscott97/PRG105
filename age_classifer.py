cc410a62514fccc18cd7226219b08f86c613c339
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-09-13 10:02:33 -0500

    Add files via upload

---
+++ age_classifer.py
@@ -0,0 +1,9 @@
+number = int(input("please emter your age "))
+if number <= 1:
+    print("you are a infant")
+elif number < 13:
+    print("You are a child")
+elif number < 20:
+    print("You are a teenager")
+elif number >= 20:
+    print ("You are a adult")
---
+++ days_week.py
@@ -0,0 +1,17 @@
+number = int(input("Give a number between 1 though 7 "))
+if number == 1:
+    print("monday")
+elif number== 2:
+    print("tuesday")
+elif number== 3:
+    print("wednesday")
+elif number== 4:
+    print("thursday")
+elif number== 5:
+    print("Friday")
+elif number== 6:
+    print("Saturday")
+elif number== 7:
+    print ("Sunday")
+else:
+    print(str(number) + " That is not a vaild number, please enter a vaild number")
