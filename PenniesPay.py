3a9043834270efd3558f411d7ba7d26605fd6f26
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-09-18 10:48:23 -0500

    Add files via upload

---
+++ PenniesPay.py
@@ -0,0 +1,13 @@
+numberOfDaysWorked = int(input("Please neter how many days you worked?"))
+totalNumberOfpennies = 0
+print()
+
+print("day\Salary)
+    for currentDay in range(numberOfDaysWorked)
+      penniesForTheday = 2 ** currentDay
+      totalNumberOfPennies += penniesForTheDay
+      print (currentDay + 1, penniesForTheDay)
+
+totalPay = totalNumberOfpennies * 0.01
+
+print("/Total pay = $, totalPay sep =")
