d0bd1de6a6db6b7c8d2b0197c01d0dfcc5e9ee2e
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-09-19 22:07:31 -0500

    Add files via upload

---
+++ Rain_fall.py
@@ -0,0 +1,19 @@
+totalNumberOfMonths = 0
+TotalInchesOfRainfall = 0
+userNumberOfYears = int(input("Please enter the number of years "))
+
+for currentYear in range (1, userNumberOfYears + 1):
+
+    for currentMonth in range(1,13):
+        monthlyRainfallInches = float(input("Please type the inches " + \
+                    "Rainfall for month " + format (currentMonth, "d") + \
+                    "Year " + format (currentYear, "d") +": " ))
+        TotalInchesOfRainfall += monthlyRainfallInches
+        totalNumberOfMonths += 1
+averageRainfall = TotalInchesOfRainfall / totalNumberOfMonths
+
+print ()
+
+print ("Number of months" + format (totalNumberOfMonths, "d",), "total" + \
+        "inches of rainfall" + format (TotalInchesOfRainfall, ".2f"),
+        "Average rainfall " + format (averageRainfall, ".2f"), )
