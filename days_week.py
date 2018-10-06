19e4de2a6d344862bce4fd16fc7abb2f5345f067
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-10-05 20:50:29 -0500

    Update days_week.py

--- days_week.py
+++ days_week.py
@@ -1,17 +1,17 @@
 number = int(input("Give a number between 1 though 7 "))
 if number == 1:
     print("monday")
-elif number== 2:
+elif number == 2:
     print("tuesday")
-elif number== 3:
+elif number == 3:
     print("wednesday")
-elif number== 4:
+elif number == 4:
     print("thursday")
-elif number== 5:
+elif number == 5:
     print("Friday")
-elif number== 6:
+elif number == 6:
     print("Saturday")
-elif number== 7:
+elif number == 7:
     print ("Sunday")
 else:
     print(str(number) + " That is not a vaild number, please enter a vaild number")
