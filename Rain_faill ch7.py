30e4329eb9b8909aa07d7bd0bf33c81ba44ed972
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-10-09 02:31:00 -0500

    Rain_fall ch7.py

---
+++ Rain_faill ch7.py
@@ -0,0 +1,18 @@
+Def main():
+Months = [ Jan, feb, March, April, May, June, July, aug, sep, oct, nov, dec]
+
+TotalInchesOfRainfall = 0
+
+    float(input("Please type the inches " +
+                                            "Rainfall for month " )
+                                         
+        
+     TotalInchesOfRainfall += monthlyRainfallInches
+        totalNumberOfMonths += 1
+averageRainfall = TotalInchesOfRainfall / totalNumberOfMonths
+
+print ()
+
+print ("Number of months" + format(totalNumberOfMonths, "d", ), "total" +
+       "inches of rainfall" + format(TotalInchesOfRainfall, ".2f"),
+       "Average rainfall " + format(averageRainfall, ".2f"),)
