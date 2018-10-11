2ea0a5c3b8b5fe471886b835b4ee75a153342bbb
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-10-11 09:42:19 -0500

    Update Automoto_loan.py

--- Automoto_loan.py
+++ Automoto_loan.py
@@ -1,39 +1,43 @@
-def askForExpenses():
-    monthlyLoanPayment = float( input("How  much do you pay for your loan" +  "every month:"))
+def ask_for_expenses():
+    monthly_loan_payment = float(input("How  much do you pay for your loan" + "every month:"))
 
-    monthlyInsurancePayment = float( input( "How much do you pay for your insurance" + "Every month:"))
+    monthly_insurance_payment = float(input("How much do you pay for your insurance" + "Every month:"))
 
-    monthlyGasPayment = float( input("How much do you pay " + "For Gas every month: "))
+    monthly_gas_payment = float(input("How much do you pay " + "For Gas every month: "))
 
-    monthlyOilPayment = float( input("How much do you pay " + "For Oil each Month:"))
+    monthly_oil_payment = float(input("How much do you pay " + "For Oil each Month:"))
 
-    monthlyTirePayment = float( input("How much do you pay for Tries" + "Each month:"))
+    monthly_tire_payment = float(input("How much do you pay for Tries" + "Each month:"))
 
-    monthlyMaintenacePayment = float( input("How much do you pay for maintenace " + "Each Month:"))
+    monthly_maintenace_payment = float(input("How much do you pay for maintenace " + "Each Month:"))
 
-    return monthlyLoanPayment, monthlyInsurancePayment, monthlyGasPayment, monthlyGasPayment, monthlyOilPayment, \
-            monthlyTirePayment, monthlyMaintenacePayment
-def calculateTotalMonthlyExpenses(payment1, payment2, payment3, payment4, payment5, payment6):
+    total = calculate_total_monthly_expense(monthly_loan_payment, monthly_insurance_payment, monthly_gas_payment, monthly_gas_payment,monthly_oil_payment ,
+           monthly_tire_payment, monthly_maintenace_payment)
+    return total
 
-    calculateTotalMonthlyExpenses = payment1 + payment2 + payment3 + payment4 + payment5 + payment6
-    return TotalMonthlyExpenses
 
-def calculateTotalAnnualCost( totalMonthlyCost):
-    totalAnnualCost = totalMonthlyCost = 12
-    return totalAnnualCost
-def printDetails(totalMonthlyCost, totalAnnualCost):
+def calculate_total_monthly_expense(payment1, payment2, payment3, payment4, payment5, payment6, payment7, ):
+    calculate_total_monthly_expenses = payment1 + payment2 + payment3 + payment4 + payment5 + payment6 + payment7
+    print(calculate_total_monthly_expenses)
+    return calculate_total_monthly_expenses
+
+
+def calculate_total_annual_cost(total_monthly_cost):
+    total_annual_cost = total_monthly_cost * 12
+    print(total_annual_cost)
+    return total_annual_cost
+
+
+def printDetails(total_monthly_cost, total_annual_cost ):
     print()
     print("Your total monthly cost is $" + \
-          format (totalMonthlyCost, ",. 2f") + "\nYour total" + \
-          "annual cost is $" + format(totalAnnualCost, ", . 2f"))
-def main():
-    loanPayment, insurancePayment, gasPayment, oilPayment, tirePayment, \
-    maintenacePayment  = askForExpenses()
+          format(total_monthly_cost, ",.2f") + "\nYour total" + \
+          "annual cost is $" + format(total_annual_cost, ",.2f"))
 
-    totalMonthlyCost = calculateTotalMonthlCost( loanPayment, insurancePayment, gasPayment, oilPayment, tirePayment,
-                                                 maintenacePayment)
-    TotalAnnualCost = calculateTotalAnnualCost()
-    printDetails()
-main()
+
+def main():
+    expenses = ask_for_expenses()
+    total_annual_cost = calculate_total_annual_cost(expenses)
+    printDetails(expenses, total_annual_cost)
 
 
