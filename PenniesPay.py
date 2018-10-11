2bf8e579834d5626867997f3ac5a3c2bd75a568b
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-10-11 09:12:31 -0500

    Update PenniesPay.py

--- PenniesPay.py
+++ PenniesPay.py
@@ -1,12 +1,12 @@
-numberOfDaysWorked = int(input("Please neter how many days you worked?"))
+numberOfDaysWorked = int(input("Please enter how many days you worked ?"))
 totalNumberOfpennies = 0
 print()
 
-print("day\Salary)
-    for currentDay in range(numberOfDaysWorked)
-      penniesForTheday = 2 ** currentDay
-      totalNumberOfPennies += penniesForTheDay
-      print (currentDay + 1, penniesForTheDay)
+print("day\tSalary\n----\t----")
+for currentDay in range(numberOfDaysWorked):
+    penniesForTheday = 2 ** currentDay
+totalNumberOfpennies += penniesForTheday
+print(currentDay + 1,"\t", penniesForTheday)
 
 totalPay = totalNumberOfpennies * 0.01
 
