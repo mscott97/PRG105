a41b3f0ee6d02deb91b24242f6695e6b9a383c48
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-10-25 00:01:36 -0500

    Add files via upload

--- Calories.py
+++ Calories.py
@@ -1,16 +1,61 @@
-def calculateCaloriesFromFat( fatGrams):
-    caloriesFromFat = fatGrams + 9
-    return caloriesFromFat
-def calculateCaloriesFromCrabs ( carbGrams):
-    caloriesFromCarbs = carbGrams + 4
-    return caloriesFromCarbs
+"""
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section that explain the required code
+    Your file should compile error free
+    Submit your completed file
+
+    We are going to convert the coin class to a dice class
+    The following code is based on the coin class from your book
+"""
+
+import random
+# TODO change class name to Dice
+
+
+class Dice:  # note class names are capitalized
+    def __init__(self):
+        # TODO change side_up to '1'
+        self.one = '1'
+        self.two = '2'
+        self.three = '3'
+        self.four = '4'
+        self.five = '5'
+        self.six = '6'
+
+        # TODO change name to roll
+    def roll(self):
+        # TODO change to reflect 6 sides and assign the number
+        if random.randint(0, 7) == 0:
+            self.one = '1'
+        elif random.randint(0, 7) == 1:
+            self.two = '2'
+        elif random.randint(0, 7) == 2:
+            self.three = '3'
+        elif random.randint(0, 7) == 3:
+            self.four = '4'
+        elif random.randint(0,7) == 4:
+            self.five = '5'
+        else:
+            self.six = '6'
+
+    def get_one(self):
+        return self.one
+
+
 def main():
-    userFatGrams = float(input("Enter Fat Grams:"))
-    userCarbsGrams = float(input("Enter Crab Grams"))
+    # TODO change my_coin to my_dice, my_dice_two and the appropriate class name throughout main
+    my_dice = Dice()
+    my_dice_two = Dice()
+    print('This side is up, ', my_dice.get_one())
+    print('This side is up, ', my_dice_two.get_one())
 
-    caloriesFromFat = calculateCaloriesFromFat( userFatGrams)
-    caloeiesFromCrabs = calculateCaloriesFromCrabs( userCarbsGrams)
-    print("\nCalories from Fat:" + format(caloriesFromFat, ".2f") + "Calories ", "Calories from crabs:" +
-          format(caloeiesFromCrabs, ".2f") + "calories", "\n")
+    print('I am rolling the dice...')
+    my_dice.roll()
+    my_dice_two.roll()
+    print('This side is up, ', my_dice.get_one())
+    print('This side is up, ', my_dice_two.get_one())
+
+
+
 main()
-
