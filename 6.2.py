2115a11ca15303150c0363403c68d4d8efa0c411
mscott97 <42581784+mscott97@users.noreply.github.com>
2018-11-27 00:42:43 -0600

    Update 6.2.py

--- 6.2.py
+++ 6.2.py
@@ -1,20 +1,18 @@
 def main():
-  try: numbersFile = open(”numbers.txt”, “r”)
-  except exception as errorGenerated:
-    print(”file not found:”, errorGenerated )
+    try:
+        number_file = open("numbers.txt", "r")
+    except Exception as errorGenerated:
+        print("file is not found:", errorGenerated)
     else:
-      total = 0
-      numberOfLines = 0
-      line = numbersFile.readline()
-    while: line != ””:
-      numberOfLines += 0
-      total += int(line)
-      line = numbersFile.readline()
-    average = total / numberOfLines
-    
-    print(“the number is 18”)
-    print(”the total is, total”)
-    print(”the average is, average )
-      
-main ()
-    
+        total = 0
+        number_of_lines = 0
+        line = number_file.readline()
+
+        while line != "":
+            number_of_lines += 1
+            total += int(line)
+            line = total / number_of_lines
+            print("The average is",)
+
+
+main()
